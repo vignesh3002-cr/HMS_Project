3ea3d08ca613b18838f3cf43888e99d753b66812
Vignesh Vicky <vigneshvickey2004@gmail.com>
2026-08-13 17:57:20 +0530

    to pull frontend

--- client/routes/ProtectedRoute.tsx
+++ client/routes/ProtectedRoute.tsx
@@ -2,72 +2,73 @@ import { Navigate } from "react-router-dom";
 import { getToken, remove } from "../utils/token";
 import { useEffect, useState } from "react";
 import api from "../api/axios";
-
-
+ 
+ 
 interface Props {
     children: JSX.Element;
 }
-
-
+ 
+ 
 export default function ProtectedRoute({ children }: Props) {
-
+ 
     const [loading, setLoading] = useState(true);
     const [valid, setValid] = useState(false);
-
-
+ 
+ 
     useEffect(()=>{
-
+ 
         const checkToken = async()=>{
-
+ 
             const token = getToken();
-
+ 
             if(!token){
                 setValid(false);
                 setLoading(false);
                 return;
             }
-
-
+ 
+ 
             try{
-
+ 
                 await api.get("/auth/me");
-
+ 
                 setValid(true);
-
+ 
             }catch(error){
-
+ 
                 remove();
-
+ 
                 setValid(false);
-
+ 
             }
-
-
+ 
+ 
             setLoading(false);
-
+ 
         };
-
-
+ 
+ 
         checkToken();
-
+ 
     },[]);
-
-
-
+ 
+ 
+ 
     if(loading){
-
+ 
         return null;
-
+ 
     }
-
-
+ 
+ 
     if(!valid){
-
+ 
         return <Navigate to="/" replace />;
-
+ 
     }
-
-
+ 
+ 
     return children;
-
+ 
 }
+ 
